ca1672bbd420fa9986cd3b51fb199d8135d4a296
Olga Lazarenko <sanjellsujeyjee@gmail.com>
2023-02-21 20:31:33 +0200

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css'
-import {BrowserRouter, Link, Route, Router, Routes} from "react-router-dom";
-import Navbar from "./pages/Home/Navbar/navbar";
-import home from "./pages/Home/home";
-import about from "./pages/About/about";
-import Home from "./pages/Home/home";
-import About from "./pages/About/about";
-import Login from "./pages/Login/login";
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter, Link, Route, Router, Routes,
+} from 'react-router-dom';
+import Navbar from './pages/Home/Navbar/navbar';
+
+import Home from './pages/Home/home';
+import About from './pages/About/about';
+import Login from './pages/Login/login';
+
 function App() {
-    return (
-        <BrowserRouter>
-            <div>
-                <Navbar />
-                <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/login" element={<Login/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 export default App;
